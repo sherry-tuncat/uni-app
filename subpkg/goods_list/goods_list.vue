4f46e40b5f20a92b3994a4482384df0c2bf2062f
weixia@axoz.cn <sherry_tuncat@163.com>
2024-12-03 23:55:38 +0800

    feat:增加商品列表功能

--- subpkg/goods_list/goods_list.vue
+++ subpkg/goods_list/goods_list.vue
@@ -1,19 +1,114 @@
 <template>
 	<view>
-		goods_list
+		<view class="list">
+			<view v-for="(item,index) in goodsList" :key="index" @click="goDetail(item)">
+				<view class="box">
+					<!-- 左侧 -->
+					<view class="">
+						<image :src="item.goods_small_logo" mode="widthFix"></image>
+					</view>
+					<!-- 右侧 -->
+					<view class="right">
+						<view class="title">
+							{{item.goods_name}}
+						</view>
+						<view class="">
+							<view class="money">
+								¥ {{Number(item.goods_price).toFixed(2) }}
+							</view>
+						</view>
+					</view>
+				</view>
+			</view>
+		</view>
 	</view>
 </template>
 
 <script>
 	export default {
+		
 		data() {
 			return {
-				
+				queryObj:{
+					query:'',
+					cid:'',
+					pagenum:1,
+					pagesize:10
+				},
+				goodsList:[],
+				total:0,
+				isLoading:false
 			};
+		},
+			
+		onLoad(options) {
+			this.queryObj = {
+				...this.queryObj,
+				...options
+			}
+			this.getGoodsList();
+		},
+		methods:{
+			async getGoodsList(cb){
+				this.isLoading = true;
+				// 发请求
+				const res = await uni.$http.get('/api/public/v1/goods/search',this.queryObj)
+				if(res.data.meta.status!==200) {
+					return uni.$showMsg()
+				}
+				this.goodsList = [...this.goodsList,...res.data.message.goods]
+				this.total = res.data.message.total
+				this.isLoading = false;
+				if(cb) {cb()}
+			},
+			goDetail(item) {
+				uni.navigateTo({
+					url:`/subpkg/goods_detail/goods_detail?goods_id=${item.goods_id}`
+				})
+			},
+			
+		},
+		onReachBottom(){
+			if(this.isLoading) return
+			if(this.goodsList.length>=this.total) return uni.showToast({title:'已经到底啦!'})
+			this.queryObj.pagenum+=1;
+			this.getGoodsList()
+		},
+		onPullDownRefresh() {
+			this.queryObj.pagenum = 1;
+			this.total = 0;
+			this.isLoading = false;
+			this.goodsList = [];
+			this.getGoodsList(()=>uni.stopPullDownRefresh() )
 		}
 	}
 </script>
 
 <style lang="scss">
-
+.list {
+	background-color: #fff;
+}
+.box {
+	display: flex;
+	padding: 5px 10px;
+	border-bottom: 1px solid #efefef;
+	max-height: 140px;
+	image {
+		width: 120px;
+		margin-right: 10px;
+	}
+	.right {
+		display: flex;
+		flex-direction: column;
+		justify-content: space-between;
+	}
+	.title {
+		font-size: 14px;
+	}
+	.money {
+		font-size: 18px;
+		font-weight: bold;
+		color: #9c6901;
+	}
+}
 </style>
